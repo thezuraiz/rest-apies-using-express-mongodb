1ffa9796c17fcfc45dc129fdb2e5eae6f76fb9eb
The Zuraiz <thezuraiz@gmail.com>
2025-04-24 13:08:19 +0500

    fix bugs for typescript server - NODE V19

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import app from "./src/app";
-
-const port = process.env.PORT || 5000;
+import { config } from "./src/config/config";
 
 const startServer = () => {
+  const port = config.port || 3000;
+
   app.listen(port, () => {
-    console.log(`server is running on ${port}`);
+    console.log("Server is running on port: ", port);
   });
 };
 
--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import express from "express";
 const app = express();
 
 // Routes
-// Http methods: GET / POST / PATCH / DELETE
-
 app.get("/", (req, res, next) => {
   res.json({ message: "Welcome Zuraiz" });
 });
