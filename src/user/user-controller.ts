dcd31611a48eaf4530734efc3466692fc6d2baba
The Zuraiz <thezuraiz@gmail.com>
2025-04-25 19:07:15 +0500

    change status code to 201

--- src/user/user-controller.ts
+++ src/user/user-controller.ts
@@ -4,44 +4,49 @@ import userModel from "./user-model";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import TUser from "./user-types";
 
 const userRegisterController = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const { email, password, name } = req.body;
+  try {
+    const { email, password, name } = req.body;
 
-  // Validation
-  if (!email || !password || !name) {
-    // Todo: Express Validator
-    const error = createHttpError(500, "validation error");
-    return next(error);
+    // Validation
+    if (!email || !password || !name) {
+      // Todo: Express Validator
+      const error = createHttpError(500, "validation error");
+      return next(error);
+    }
+
+    // Find existing user
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(300, "User email already registered");
+      return next(error);
+    }
+
+    // Encrypt the password
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const newUser: TUser = await userModel.create({
+      name,
+      email,
+      password: hashedPass,
+    });
+
+    // JWT Token NOTE: "Sign" ko jsonwebtoken sy import krna hy
+    const token = await sign({ sub: newUser._id }, config.secret as string, {
+      expiresIn: "7d",
+      // algorithm: "HS256", bby default ye value pakarta hy
+    });
+
+    res.status(201).json({ accessToken: token });
+  } catch (e) {
+    return next(createHttpError(500, `something went wrong ${e}`));
   }
-
-  // Find existing user
-  const user = await userModel.findOne({ email });
-  if (user) {
-    const error = createHttpError(300, "User email already registered");
-    return next(error);
-  }
-
-  // Encrypt the password
-  const hashedPass = await bcrypt.hash(password, 10);
-
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPass,
-  });
-
-  // JWT Token NOTE: "Sign" ko jsonwebtoken sy import krna hy
-  const token = await sign({ sub: newUser._id }, config.secret as string, {
-    expiresIn: "7d",
-    // algorithm: "HS256", bby default ye value pakarta hy
-  });
-
-  res.status(200).json({ accessToken: token });
 };
 
 export { userRegisterController };
